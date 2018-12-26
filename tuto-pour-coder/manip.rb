d2c3bc7b8899ffb287b75e9578e05165805fab48
davidSoutarson <triplaxs@gmail.com>
2018-12-27 00:36:23 +0100

    initialize un nouvau tp

--- tuto-pour-coder/manip.rb
+++ tuto-pour-coder/manip.rb
@@ -1,13 +1,17 @@
 class Utilisateur
   attr_accessor :prenom
   
-  def initialize(prenom)
-    @prenom = prenom 
+  def initialize(prenom, sexe ) 
+    if sexe == "f"
+     @prenom = "Madame " + prenom 
+    else 
+     @prenom = "Monsieur " + prenom 
+    end 
   end 
 end
 
-alice = Utilisateur.new("Alice")
-bob = Utilisateur.new("Bob")
+alice = Utilisateur.new("Alice","f")
+bob = Utilisateur.new("Bob","h") 
 puts alice.prenom 
 puts bob.prenom
 
