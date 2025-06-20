c88bfb6dcb7a2a309eb3f93fd68c2634850b6dbb
Soutarson David <45629054+davidSoutarson@users.noreply.github.com>
2025-06-20 19:08:48 +0200

    feat: héritage des classes Chat et Chien depuis Animal
    Implémentation de l'héritage en Ruby.
    Chat et Chien héritent de Animal, chacun avec sa propre méthode parler.
    Affichage personnalisé selon le type d'animal.

--- tuto-pour-coder/heritage.rb
+++ tuto-pour-coder/heritage.rb
@@ -1,26 +1,35 @@
+# Classe de base Animal avec un attribut nom
 class Animal
-  attr_accessor :nom 
-  
+  attr_accessor :nom
+
   def initialize(nom)
-    @nom = nom 
-  end 
-end
-#la class chat herite de la class Animal 
-class Chat < Animal
-  def parler 
-    puts "#{nom}: Miaou!"
-  end  
-end 
-#la class chien herite de la class Animal 
-class Chien < Animal
-  def parler 
-    puts "#{nom}: Ouaf!"
+    @nom = nom
   end
 end
-#variable pour atribuant les parametre () a nom
+
+# La classe Chat hérite de Animal
+class Chat < Animal
+  def parler
+    puts "#{nom} : Miaou !"  # Le chat parle
+  end
+end
+
+# La classe Chien hérite aussi de Animal
+class Chien < Animal
+  def parler
+    puts "#{nom} : Ouaf !"  # Le chien parle
+  end
+end
+
+# Création d'instances avec un nom
 mon_chien = Chien.new("Bob le chien")
 mon_chat = Chat.new("Adeline le chat")
-#execution de la method parler 
+
+# Appel de la méthode parler pour chaque animal
 mon_chat.parler
 mon_chien.parler
 
+
+# Résultat attendu :
+# Adeline le chat : Miaou !
+# Bob le chien : Ouaf !
