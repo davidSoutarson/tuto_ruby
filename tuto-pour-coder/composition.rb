7968c590d538562affb98929e8b2474b2aa71dda
Soutarson David <45629054+davidSoutarson@users.noreply.github.com>
2025-06-20 18:54:37 +0200

    feat: ajout des classes Eleve et Examen avec affichage des notes
    Création de deux classes pour modéliser un élève et ses examens.
    Initialisation d'un élève avec deux matières.
    Affichage dynamique des notes avec .each.
    Correction d'une faute de frappe dans "Examens".

--- tuto-pour-coder/composition.rb
+++ tuto-pour-coder/composition.rb
@@ -1,25 +1,32 @@
+# Définition d'une classe Eleve, avec un nom et une liste d'examens
 class Eleve
   attr_accessor :nom, :examens
 end
 
+# Définition d'une classe Examen avec un nom et une note
 class Examen
   attr_accessor :nom, :note
 
+  # Initialisation de l'examen avec ses attributs
   def initialize(nom, note)
-    @nom = nom 
-    @note = note 
+    @nom = nom
+    @note = note
   end
-end 
+end
 
+# Création d'un élève nommé Billy
 mon_eleve = Eleve.new
 mon_eleve.nom = "Billy"
 
-maths = Examen.new("maths",10)
+# Création de deux examens avec leur note
+maths = Examen.new("maths", 10)
 philo = Examen.new("philo", 15)
 
+# Assignation des examens à l'élève
 mon_eleve.examens = [maths, philo]
 
-puts "Esamens de #{mon_eleve.nom}"
+# Affichage des examens de l'élève
+puts "Examens de #{mon_eleve.nom} :"
 mon_eleve.examens.each do |examen|
-  puts examen.nom
+  puts "- #{examen.nom} : #{examen.note}/20"
 end
